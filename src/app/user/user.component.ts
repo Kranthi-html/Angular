55ebde04782dd4bd302d95fb967593b8e9bab738
Kranthi-html <kranthi.akepogu@wissen.com>
2025-07-07 20:37:32 +0530

    Day3_angular

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, EventEmitter, Input,input, Output,output}  from '@angular/core';
+ //import {computed,signal} from '@angular/core';
+// import { DUMMY_USERS } from '../dummy-users';
+//const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +10,45 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+ @Input({required:true}) id!:string ;
+ @Input({required:true}) avatar!:string ;
+ @Input({required:true}) name!:string;
+
+ @Output() select = new EventEmitter();
+
+ /**
+  * @Output() select = new EventEmitter<string>(); 
+  *  this is also allowed with strict ype 
+  */
+ //   this will be used with Output decerator  
+ 
+  //select = output<string>();
+
+  // for output() , new need to create an eventemmiter object , it already created by default
+
+//   avatar = input.required<string>();
+//   name = input.required<string>();
+//   imagePath = computed(()=>{
+// return 'assets/users/' + this.avatar();
+//   });
+ get imagePath() {
+    return 'assets/users/' + this.avatar
+  }
+   onSelectUser(){
+    this.select.emit(this.id);
+    ///  this.avatar.set();
+   }
+
+  // selectedUser = signal(DUMMY_USERS[randomIndex]); 
+  
+  // imagePath = computed(()=> 'assets/users/' + this.selectedUser().avatar)
+  // // get imagePath() {
+  // //   return 'assets/users/' + this.selectedUser.avatar
+  // // }
+  // onSelectUser(){
+  //  const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+  //  this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  //  /* this.selectedUser = DUMMY_USERS[randomIndex];*/
+  // }
 
 }
