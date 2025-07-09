21ed5323b37ae3509c47be5688779023007d0625
Kranthi-html <kranthi.akepogu@wissen.com>
2025-07-09 23:08:22 +0530

    Day4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 import {UserComponent} from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UserComponent,TasksComponent],
+  imports: [UserComponent,TasksComponent,NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-   selectedUserId = 'u1';
+   //selectedUserId = 'u1';
+       selectedUserId?:string;
    get selectedUser(){
     return this.users.find((user)=>
       user.id === this.selectedUserId);
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,18 @@ import { Component, computed, EventEmitter, Input,input, Output,output}  from '@
  //import {computed,signal} from '@angular/core';
 // import { DUMMY_USERS } from '../dummy-users';
 //const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+// type User = {
+//   id : string;
+//   avatar : string;
+//   name : string;
+// };
+
+  interface User {
+    id : string;
+   avatar : string;
+   name : string;
+  }
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,9 +22,11 @@ import { Component, computed, EventEmitter, Input,input, Output,output}  from '@
   styleUrl: './user.component.css'
 })
 export class UserComponent {
- @Input({required:true}) id!:string ;
- @Input({required:true}) avatar!:string ;
- @Input({required:true}) name!:string;
+//  @Input({required:true}) id!:string ;
+//  @Input({required:true}) avatar!:string ;
+//  @Input({required:true}) name!:string;
+
+@Input({required:true}) user!:User;
 
  @Output() select = new EventEmitter();
 
@@ -32,10 +46,10 @@ export class UserComponent {
 // return 'assets/users/' + this.avatar();
 //   });
  get imagePath() {
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
    onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
     ///  this.avatar.set();
    }
 
