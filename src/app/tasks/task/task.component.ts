05748013c6c35626a91f0f768feded67c34f0ffa
Kranthi-html <kranthi.akepogu@wissen.com>
2025-07-11 18:25:49 +0530

    code clean up

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,7 +1,8 @@
-import { Component,EventEmitter,Input, Output } from '@angular/core';
+import { Component,EventEmitter,Input, Output ,inject} from '@angular/core';
 import { Task } from './task.model';
 import { CardComponent } from '../../shared/card/card.component';
 import { DatePipe } from '@angular/common';
+import { TasksService } from './tasks.service';
 @Component({
   selector: 'app-task',
   standalone: true,
@@ -13,6 +14,7 @@ export class TaskComponent {
  @Input({required:true}) task! :Task 
  @Output() complte = new EventEmitter<string>();
 
+ private taskService = inject(TasksService);
  onCompleteTask() {
    this.complte.emit(this.task.id);
  }
