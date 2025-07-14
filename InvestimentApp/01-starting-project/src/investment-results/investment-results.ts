9b890cb4e8ab4ee09dd2e84f4e61d2a54c961afd
Kranthi-html <kranthi.akepogu@wissen.com>
2025-07-14 23:36:06 +0530

    Day7

---
+++ InvestimentApp/01-starting-project/src/investment-results/investment-results.ts
@@ -0,0 +1,19 @@
+import { Component ,Input,input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  imports: [],
+  templateUrl: './investment-results.html',
+  styleUrl: './investment-results.css'
+})
+export class InvestmentResults {
+  // results = input()
+  @Input() results? : {
+      year: number,
+      interest: number,
+      valueEndOfYear: number,
+      annualInvestment: number,
+      totalInterest: number,
+      totalAmountInvested: number
+    }[];
+}
