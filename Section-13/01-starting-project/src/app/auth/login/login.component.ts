f5100b3ebde3380146761161c8648bf14243f1b4
Kranthi-html <kranthi.akepogu@wissen.com>
2025-07-17 18:59:57 +0530

    Day10-Reactive forms

--- Section-13/01-starting-project/src/app/auth/login/login.component.ts
+++ Section-13/01-starting-project/src/app/auth/login/login.component.ts
@@ -1,8 +1,20 @@
 
 
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators,AsyncValidatorFn  } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+function mustContainQuestionMark(control : AbstractControl){
+    if(control.value.includes('?')) {
+      return null;
+    }
+      return {doesNotContainQuestionMark : true}
+  }
+ function emailIsUnique(control :AbstractControl) {
+      if(control.value !== 'test@example.com' ) {
+           return of(null);
+      }
+      return of({notUnique :true});
+ } 
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -14,11 +26,24 @@ export class LoginComponent {
   form = new FormGroup({
     email: new FormControl('',{
       validators:[Validators.email,Validators.required],
+      asyncValidators:[emailIsUnique]
     }),
     password: new FormControl('',{
-      validators:[Validators.required,Validators.minLength(6)]
+      validators:[Validators.required,Validators.minLength(6), mustContainQuestionMark],
     }),
   });
+
+  get emailIsInvalid() {
+      return (this.form.controls.email.touched && 
+               this.form.controls.email.dirty &&
+                this.form.controls.email.invalid);
+  }
+
+  get passwordIsInvalid() {
+      return (this.form.controls.password.touched && 
+               this.form.controls.password.dirty &&
+                this.form.controls.password.invalid);
+  }
    onSubmit(){
      //this.form.value.email
 
